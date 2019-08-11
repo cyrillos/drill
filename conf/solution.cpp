eb8b86691b90a148735cf6f72d26042a2475781e
Cyrill Gorcunov <gorcunov@gmail.com>
2019-08-12 00:35:06 +0300

    drill Update cpp proto
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ conf/solution.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v)
+{
+	out << '[';
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if (it != v.begin())
+			out << ',';
+		out << *it;
+	}
+	out << ']';
+	return out;
+}
+
+class Solution {
+public:
+};
+
+int main()
+{
+	vector<int> input[] = {
+		{}, // 0
+	};
+	Solution solution;
+	for (const auto& nums: input) {
+		cout << nums << " => " <<
+			solution.name(nums) << endl;
+	}
+	return 0;
+}
