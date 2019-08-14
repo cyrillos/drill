dc78437876a7814a377b46b204134b7409d14e23
Cyrill Gorcunov <gorcunov@gmail.com>
2019-08-14 20:37:38 +0300

    update template
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- conf/solution.cpp
+++ conf/solution.cpp
@@ -18,6 +18,19 @@ ostream& operator<<(ostream& out, const vector<T>& v)
 	return out;
 }
 
+template<typename K, typename V>
+ostream& operator<<(ostream& out, unordered_map<K,V> v)
+{
+       out << '{';
+       for (auto it = v.begin(); it != v.end(); ++it) {
+               if (it != v.begin())
+                       out << ", ";
+               out << '[' << it->first << "]: " << it->second;
+       }
+       out << '}';
+       return out;
+}
+
 class Solution {
 public:
 };
