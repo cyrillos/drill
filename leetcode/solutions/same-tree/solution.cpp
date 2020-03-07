9808378b0367dbd9cbda79c28859b5307b2a1931
Cyrill Gorcunov <gorcunov@gmail.com>
2020-03-07 16:19:58 +0300

    same-tree
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ leetcode/solutions/same-tree/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (p == NULL || q == NULL) {
+			if (p == NULL && q == NULL)
+				return true;
+			return false;
+		}
+
+		if (p->val != q->val)
+			return false;
+		
+		// Trivial dfs
+		return isSameTree(p->left, q->left) &&
+			isSameTree(p->right, q->right);
+	}
+};
