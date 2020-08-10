6b3ce2f3c9d37fd134fa01bac7cb2158f5701f49
Cyrill Gorcunov <gorcunov@gmail.com>
2020-08-10 23:40:08 +0300

    3sum: add some comments
    for easier memory refresh
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- leetcode/solutions/3sum/solution.cpp
+++ leetcode/solutions/3sum/solution.cpp
@@ -36,32 +36,51 @@ public:
 	vector<vector<int>> threeSum(vector<int>& nums) {
 		vector<vector<int>> res;
 
+		//
+		// Key moment to work on sorted data: searching.
 		sort(nums.begin(), nums.end());
 
 		//
 		// This is gonna be O(n^2) algo
 		//
+		// num1 num2 num3 num4
+		// ^    ^         ^
+		// pos  i         j
+		//
 		for (ssize_t pos = 0; pos < (ssize_t)nums.size() - 2;) {
 			ssize_t j = nums.size() - 1;
 			ssize_t i = pos + 1;
 
+			//
+			// Iterate from [i:j] range with pos fixed
 			while (i < j) {
 				int sum = nums[pos] + nums[i] + nums[j];
+				//
+				// found exact
 				if (sum == 0) {
 					vector<int> v({nums[pos], nums[i], nums[j]});
 					res.push_back(move(v));
 				}
 
+				//
+				// skip same numbers (since we know
+				// it must have only one solution)
 				if (sum <= 0) {
+					// if sum is not enough
 					do {
 						i++;
 					} while (i < j && nums[i] == nums[i-1]);
 				} else if (sum >= 0) {
+					// if we're too big
 					do {
 						j--;
 					} while (i < j && nums[j] == nums[j+1]);
 				}
 			}
+
+			//
+			// skip same numbers (since we know
+			// it must have only one solution)
 			do {
 				pos++;
 			} while (pos < (ssize_t)nums.size() && nums[pos] == nums[pos-1]);
