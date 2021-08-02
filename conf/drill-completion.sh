ffbb290ccef06350a75b6f8ea75346d8fbc6e053
Cyrill Gorcunov <gorcunov@gmail.com>
2021-08-02 20:49:33 +0300

    conf: completion
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- conf/drill-completion.sh
+++ conf/drill-completion.sh
@@ -15,7 +15,7 @@ _drill()
 	tag="${tag} dynamic-programming linked-list sliding-window string"
 	tag="${tag} binary-search depth-first-search hash-table linked-lists"
 	tag="${tag} sort tree union-find greedy graph prefix-sum"
-	tag="${tag} binary-indexed-tree segment-tree line-sweep"
+	tag="${tag} binary-indexed-tree binary-tree segment-tree line-sweep"
 
 	case ${COMP_CWORD} in
 	1)
