9f43824fac104df44120ae810e4c7298abc5597c
Cyrill Gorcunov <gorcunov@gmail.com>
2021-08-12 16:36:04 +0300

    lowest-common-ancestor-of-a-binary-tree: iterative
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- leetcode/solutions/lowest-common-ancestor-of-a-binary-tree/solution.cc
+++ leetcode/solutions/lowest-common-ancestor-of-a-binary-tree/solution.cc
@@ -15,11 +15,13 @@
    /     \
   5       1
  / \     / \
-6   2   0   8
+6   2*  0*  8
    / \
-  7   4
+  7*  4
 #endif
 
+#if 0
+// recursive solution
 class Solution {
 public:
 	TreeNode *parent = nullptr;
@@ -46,5 +48,66 @@ public:
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 		dfs(root, p, q);
 		return parent;
+	}
+};
+#endif
+
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		//
+		// Iterative approach:
+		//  - remember parents until both nodes are found
+		//  - traverse one path from @p to @root remembering
+		//    nodes visited in a set
+		//  - traverse second path bottom up from @q to @root
+		//    and once visited node from first path is found
+		//    return it as LCA
+		unordered_map<TreeNode *, TreeNode* > parents;
+		set<TreeNode *> path;
+		stack<TreeNode *> st;
+		int visited = 0;
+
+		parents[root] = nullptr;
+		st.push(root);
+
+		while (!st.empty()) {
+			auto node = st.top();
+			st.pop();
+
+			if (node == p)
+				visited++;
+			else if (node == q)
+				visited++;
+
+			if (node->right) {
+				parents[node->right] = node;
+				st.push(node->right);
+			}
+
+			if (node->left) {
+				parents[node->left] = node;
+				st.push(node->left);
+			}
+
+			if (visited == 2)
+				break;
+		}
+
+		auto parent_p = p;
+		auto parent_q = q;
+
+		while (parent_p != nullptr) {
+			path.insert(parent_p);
+			parent_p = parents[parent_p];
+		}
+
+		while (parent_q != nullptr) {
+			if (path.find(parent_q) != path.end())
+				return parent_q;
+			parent_q = parents[parent_q];
+		}
+
+		return nullptr;
 	}
 };
