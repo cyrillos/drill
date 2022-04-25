44a990ea4a72dad335b314da218bda16ce5ecd7a
Cyrill Gorcunov <gorcunov@gmail.com>
2022-04-25 22:55:06 +0300

    spiral-matrix
    simplify
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- leetcode/solutions/spiral-matrix/solution.cpp
+++ leetcode/solutions/spiral-matrix/solution.cpp
@@ -34,115 +34,69 @@ ostream& operator<<(ostream& out, unordered_map<K,V> v)
 
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& m) {
-	    vector<int> res;
+	vector<int> spiralOrder(vector<vector<int>>& matrix) {
+		int rows = matrix.size();
+		if (rows == 0)
+			return {};
+		int cols = matrix[0].size();
+		if (cols == 0)
+			return {};
 
-	    if (m.size() < 1 || m[0].size() < 1)
-		    return {};
+		vector<int> res(rows * cols);
 
-	    int nr_cols = m[0].size();
-	    int nr_rows = m.size();
+		int left = 0, right = cols-1, top = 0, down = rows-1;
+		int pos = 0;
+		while (pos < rows * cols) {
+			// top | left to right
+			for (int c = left; c <= right; c++)
+				res[pos++] = matrix[top][c];
+			// right | top to bottom
+			for (int r = top+1; r <= down; r++)
+				res[pos++] = matrix[r][right];
+			if (top != down) {
+				// bottom | right to left
+				for (int c = right-1; c >= left; c--)
+					res[pos++] = matrix[down][c];
+			}
+			if (left != right) {
+				// left | bottom to top
+				for (int r = down-1; r > top; r--)
+					res[pos++] = matrix[r][left];
+			}
+			left++, right--, top++, down--;
+		}
 
-	    size_t size = nr_cols * nr_rows;
-	    int pos = 0;
-
-	    if (nr_cols == 1 || nr_rows == 1) {
-		    res.resize(size);
-		    if (nr_cols == 1) {
-			    for (int i = 0; i < nr_rows; i++)
-				    res[pos++] = m[i][0];
-		    } else {
-			    for (int i = 0; i < nr_cols; i++)
-				    res[pos++] = m[0][i];
-		    }
-		    return res;
-	    }
-
-	    res.resize(size);
-
-	    int c[2] = {0, nr_cols-1};
-	    int r[2] = {0, nr_rows-1};
-
-
-	    // Say matrix is 3x3
-	    // c[] = {0, 2}
-	    // r[] = {0, 2}
-
-	    while (c[0] <= c[1] && r[0] <= r[1]) {
-		    // Horizontal forward
-		    for (int i = c[0]; i <= c[1]; i++)
-			    res[pos++] = m[r[0]][i];
-		    // First row is complete
-		    r[0]++;
-		    // r = {1, 2}
-
-		    if (c[0] > c[1] || r[0] > r[1]) break;
-
-		    // Vertical top down
-		    for (int i = r[0]; i <= r[1]; i++)
-			    res[pos++] = m[i][c[1]];
-		    // Last column is complete
-		    c[1]--;
-		    // c = {0 ,1}
-
-		    if (c[0] > c[1] || r[0] > r[1]) break;
-		    // -- || --
-
-		    // c = {0, 1}
-		    // r = {1, 2}
-
-		    // Horizontal backward
-		    for (int i = c[1]; i >= c[0]; i--)
-			    res[pos++] = m[r[1]][i];
-		    // Last row is complete
-		    r[1]--;
-		    // r = {1, 1}
-
-		    if (c[0] > c[1] || r[0] > r[1]) break;
-		    // Vertical down top
-		    for (int i = r[1]; i >= r[0]; i--)
-			    res[pos++] = m[i][c[0]];
-
-		    // Last column is complete
-		    c[0]++;
-		    // c = {1, 1}
-
-		    // c = {1, 1}
-		    // r = {1, 1}
-		    if (c[0] > c[1] || r[0] > r[1]) break;
-	    }
-
-	    return res;
-    }
+		return res;
+	}
 };
 
 int main()
 {
-	vector<vector<int>> input[] = {
-		{
-			{1, 2, 3},
-			{4, 5, 6},
-			{7, 8, 9},
-		}, {
-			{1, 2, 3, 4},
-			{5, 6, 7, 8},
-			{9,10,11,12},
-		}, {
-			{1, 2, 3 },
-			{4, 5, 6 },
-			{7, 8, 9 },
-			{10,11,12},
-		}, {
-			{1, 2},
-			{3, 4},
-		}, {
-			{6,9,7},
-		}, {
-			{6},
-			{9},
-			{7},
-		}
-	};
+        vector<vector<int>> input[] = {
+                {
+                       {1, 2, 3},
+                       {4, 5, 6},
+                       {7, 8, 9},
+               }, {
+                       {1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9,10,11,12},
+               }, {
+                       {1, 2, 3 },
+                       {4, 5, 6 },
+                       {7, 8, 9 },
+                       {10,11,12},
+               }, {
+                       {1, 2},
+                       {3, 4},
+               }, {
+                       {6,9,7},
+               }, {
+                       {6},
+                       {9},
+                       {7},
+                }
+        };
 
 	Solution solution;
 	for (auto& m: input) {
